afc3f7303d477590d934225ee0cab46c9902e20c
Anssi Kääriäinen <anssi@holvi.com>
2019-03-22 07:11:50 +0000

    Allow setting a delay for messages
    Also, a couple of minor cleanups:
      - Require message_source in register_transport()
      - Logger name to __name__ instead of '__name__'

--- outbox/service.py
+++ outbox/service.py
@@ -1,28 +1,34 @@
 from django.db import connection
+from django.utils import timezone
+from datetime import timedelta
 from .models import Outbox
 from logging import getLogger
-logger = getLogger('__name__')
+logger = getLogger(__name__)
 
 
 class OutboxService(object):
     def __init__(self):
         self.config = {}
 
-    def register_transport(self, message_type, transport):
-        self.config[message_type] = transport
+    def register_transport(self, message_source, message_type, transport):
+        self.config[(message_source, message_type)] = transport
 
-    def create_message(self, message_source, message_type, payload):
+    def create_message(self, message_source, message_type, payload,
+                       delay=0):
         from outbox.tasks import async_send
         message = Outbox.objects.create(
             message_source=message_source, message_type=message_type,
-            payload=payload)
-        connection.on_commit(
-            lambda: async_send.delay(outbox_uuid=message.uuid))
+            payload=payload,
+            next_attempt=timezone.now() + timedelta(seconds=delay))
+        if delay == 0:
+            connection.on_commit(
+                lambda: async_send.delay(outbox_uuid=message.uuid))
 
     def send(self, message, force=False):
         if not force and message.delivered_at:
             raise Exception("This message has been already delivered")
-        transport = self.get_transport(message.message_type)
+        transport = self.get_transport(
+            message.message_source, message.message_type)
         try:
             transport.send(message)
         except Exception:
@@ -32,8 +38,8 @@ class OutboxService(object):
         if message.delivered_at is None:
             transport.on_failure(message)
 
-    def get_transport(self, message_type):
-        return self.config[message_type]
+    def get_transport(self, message_source, message_type):
+        return self.config[(message_source, message_type)]
 
 
 _service = OutboxService()
